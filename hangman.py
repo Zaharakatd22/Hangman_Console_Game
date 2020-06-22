681626afb3fee22a58b7ed0710c7c8f7abdd1c61
Zaharakatd <37711267+Zaharakatd22@users.noreply.github.com>
2020-06-22 15:35:33 +0400

    Refactor to OOP paradigm

--- hangman.py
+++ hangman.py
@@ -1,2 +1,16 @@
-print("H A N G M A N")
-print("The game will be available soon.")
+class Hangman:
+    def __init__(self, title: str = "H A N G M A N", announcement: str = "The game will be available soon."):
+        self.title = title
+        self.announcement = announcement
+
+    def print_title(self):
+        print(self.title)
+
+    def print_announce(self):
+        print(self.announcement)
+
+
+hangman = Hangman()
+
+hangman.print_title()
+hangman.print_announce()
