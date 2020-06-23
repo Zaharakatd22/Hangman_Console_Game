9b92ed399a3671c09f6d5174baf97db6f6405a02
Zaharakatd <37711267+Zaharakatd22@users.noreply.github.com>
2020-06-23 09:22:53 +0400

    Add one-word one-life guess game

--- hangman.py
+++ hangman.py
@@ -2,6 +2,7 @@ class Hangman:
     def __init__(self, title: str = "H A N G M A N", announcement: str = "The game will be available soon."):
         self.title = title
         self.announcement = announcement
+        self.guess_word = "python"
 
     def print_title(self):
         print(self.title)
@@ -9,8 +10,15 @@ class Hangman:
     def print_announce(self):
         print(self.announcement)
 
+    def check_word(self, user_word: str):
+        return user_word == self.guess_word
+
 
 hangman = Hangman()
 
 hangman.print_title()
-hangman.print_announce()
+word: str = input("Guess the word: > ")
+if hangman.check_word(word):
+    print("You survived!")
+else:
+    print("You are hanged!")
